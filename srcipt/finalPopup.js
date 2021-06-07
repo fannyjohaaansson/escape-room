1315e012124a2930350b6dc978824fc4735715ae
fannyjohaaansson <fanny.johansson@hyperisland.se>
2021-06-07 13:52:46 +0200

    added function of not skipping ahead + styling

--- srcipt/finalPopup.js
+++ srcipt/finalPopup.js
@@ -1,9 +1,16 @@
 AFRAME.registerComponent('open-final', {
 	init: function () {
 		let el = this.el
+		let Cylinder = document.getElementById('cylinder')
 		let finalPopup = document.getElementById('finalClue')
+		let finalFalsePopup = document.getElementById('finalClueFalse')
+
 		this.finalInfo = function () {
-			finalPopup.classList.remove('hide')
+			if (Cylinder.classList.contains('clicked')) {
+				finalPopup.classList.remove('hide')
+			} else {
+				finalFalsePopup.classList.remove('hide')
+			}
 		}
 		this.el.addEventListener('click', this.finalInfo)
 	},
@@ -11,7 +18,13 @@ AFRAME.registerComponent('open-final', {
 		this.el.removeEventListener('click', this.finalInfo)
 	},
 })
+
 function closeFinalPopup() {
 	let finalPopup = document.getElementById('finalClue')
 	finalPopup.classList.add('hide')
 }
+function closeFinalFalsePopup() {
+	let finalFalsePopup = document.getElementById('finalClueFalse')
+	finalFalsePopup.classList.add('hide')
+}
+// if (document.getElementById('cylinder').classlist.contains('clicked')) {
--- srcipt/popup.js
+++ srcipt/popup.js
@@ -2,8 +2,10 @@ AFRAME.registerComponent('open-popup', {
 	init: function () {
 		let el = this.el
 		let Firstpopup = document.getElementById('popupWindow')
+		let Cylinder = document.getElementById('cylinder')
 		this.openPopup = function () {
 			Firstpopup.classList.remove('hide')
+			Cylinder.classList.add('clicked')
 		}
 		this.el.addEventListener('click', this.openPopup)
 	},
